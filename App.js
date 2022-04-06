b4ad91b8ac62aee6746e0d963a78d93144cf5009
alexandre <alexandre@mail.com>
2022-04-06 14:43:21 +0200

    repo

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+import { ScrollView,StyleSheet, Text, View } from 'react-native';
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  mainContainer: {
+    backgroundColor:"blue"
   },
+  textContainer:{
+    backgroundColor:"white"
+  },
+  text1: {
+    marginBottom:80,
+    marginTop:80,
+    fontSize:30
+    },
+  text2:{
+    marginBottom:80,
+    marginTop:80,
+    textAlign:"center"
+  },
+  text3:{
+    marginBottom:80,
+    marginTop:80,
+    fontWeight:"bold"  }
 });
+export default function App() {
+  
+  return (
+      <ScrollView style={styles.mainContainer}>
+        <View style={styles.textContainer}>
+          <Text style={styles.text1}>Coucou </Text>
+          <Text style={styles.text2}>Salut</Text>
+          
+          
+          <Text style={styles.text3}>Open up App.js to start working on your app!</Text>
+          <Text style={styles.text1}>Coucou </Text>
+          <Text style={styles.text2}>Salut</Text>
+          
+          
+          <Text style={styles.text3}>Open up App.js to start working on your app!</Text>
+          <Text style={styles.text1}>Coucou </Text>
+          <Text style={styles.text2}>Salut</Text>
+          
+          
+          <Text style={styles.text3}>Open up App.js to start working on your app!</Text>
+          <Text style={styles.text1}>Coucou </Text>
+          <Text style={styles.text2}>Salut</Text>
+          
+          
+          <Text style={styles.text3}>Open up App.js to start working on your app!</Text>
+        </View>
+      </ScrollView>
+  )}
